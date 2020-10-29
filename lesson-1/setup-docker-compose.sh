b0bf05bb924d78c7d7babce6b620885a32e3ddf8
stony-su <taixue@gmail.com>
2020-10-28 20:38:27 -0400

    DEV-1 test empty commit.

---
+++ lesson-1/setup-docker-compose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+# Install docker 
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
+# Install docker-compose
+sudo apt install -y jq
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest"|jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
